0253e87ad5ff2d78db4d29d8b1b7dd421892c46f
Maria Regina <santosdedeusm@gmail.com>
2021-06-24 16:23:35 -0300

    Vai 3

--- src/app/folder/ambiente/ambiente.component.ts
+++ src/app/folder/ambiente/ambiente.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Ambiente } from 'src/app/models/ambiente.model';
 import { AmbienteService } from './ambiente.service';
-import { FormBuilder, FormGroup } from '@angular/forms';
 import { AlertController } from '@ionic/angular';
 
 @Component({
@@ -10,56 +9,51 @@ import { AlertController } from '@ionic/angular';
 })
 
 export class AmbienteComponent implements OnInit {
-  public form: FormGroup;
   ambientes ?: Ambiente[];
   ambienteSelecionado ?: Ambiente;
   currentIndex = -1;
   detalharAmbiente = false;
 
   constructor(private ambienteService: AmbienteService, 
-    private formBuilder: FormBuilder,
     public alertController: AlertController) { }
-    
-  ngOnInit() {
-    this.habilitaLista();
-    this.form = this.formBuilder.group({
-      ambienteSelecionado: {},
-    });
-  }
+    ngOnInit() {
+      this.habilitaLista();
+    }
   
-  habilitaLista(): void{
-    this.ambienteService.getAll()
-      .subscribe(
-        data => {
-          this.ambientes = data;
-          this.currentIndex = 0;
-          this.detalharAmbiente = false;
-        },
-        error => {
-          console.log(error);
-        });
-  }
+    habilitaLista(): void{
+      this.ambienteService.getAll()
+        .subscribe(
+          data => {
+            this.ambientes = data;
+            this.currentIndex = 0;
+            this.detalharAmbiente = false;
+          },
+          error => {
+            console.log(error);
+          });
+    }
       
-  detalhaAmbiente(ambiente): void{
-    this.form.value.ambienteSelecionado = ambiente;
-    this.ambienteSelecionado = ambiente;
-    this.detalharAmbiente = true;
-    console.log('vai');
-  }
+    detalhaAmbiente(ambiente): void{
+      this.ambienteSelecionado = ambiente;
+      this.detalharAmbiente = true;
+      
+      console.log('vai');
+      console.log(this.ambienteSelecionado)
+    }
 
-  salvar(): void {
-    console.log('teste');
-  }
+    salvar(): void {
+      console.log('teste');
+    }
 
-  async presentAlert() {
-    const alert = await this.alertController.create({
-      header: 'Alert',
-      subHeader: 'Subtitle',
-      message: 'This is an alert message.',
-      buttons: ['OK']
-    });
+    async presentAlert() {
+      const alert = await this.alertController.create({
+        header: 'Alert',
+        subHeader: 'Subtitle',
+        message: 'This is an alert message.',
+        buttons: ['OK']
+      });
 
-    await alert.present();
-  }
+      await alert.present();
+    }
 
 }
